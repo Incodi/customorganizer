755e0c69de8a3714be9afce83e7f8f7d07fc1849
clara uhm <118067160+Incodi@users.noreply.github.com>
2025-05-15 16:50:06 +0000

    Fixed bug where undo_log.txt is put into Others folder

--- organizer.py
+++ organizer.py
@@ -3,13 +3,11 @@ from pathlib import Path
 import shutil
 
 def save_undo_log():
-    """Save current file positions for undo"""
     with open("undo_log.txt", "w") as f:
         for file in Path.cwd().rglob("*.*"):
             f.write(f"{file.parent}|{file.name}\n")
 
 def undo_last_organization():
-    """Revert last organization"""
     if Path("undo_log.txt").exists():
         with open("undo_log.txt") as f:
             for line in f:
@@ -17,9 +15,9 @@ def undo_last_organization():
                 shutil.move(Path(path).joinpath(filename), Path.cwd())
 
 directories = {
-    "ArtsandMemes": [".jpeg", ".jpg", ".gif", ".png", ".webm", ".xcf", 
+    "ArtsandMemes": [".jpeg", ".jpg", ".gif", ".png", ".webp", ".xcf", 
                      ".psd", ".ora", ".scriv", ".toml", ".gba", ".procreate", 
-                     ".artstudio", ".odp"],
+                     ".artstudio", ".odp", ".heic"],
     "Videos": [".wmv", ".mov", ".mp4", ".mpg", ".mpeg", ".mkv"], 
     "Zips": [".iso", ".tar", ".gz", ".rz", ".7z", ".dmg", ".rar", ".zip"],
     "Audio": [".mp3", ".msv", ".wav", ".wma"],
@@ -43,6 +41,9 @@ def organizer():
         file_path = Path(entry)
         final_file_format = file_path.suffix.lower()
         
+        if file_path.name == "undo_log.txt":
+            continue
+
         if final_file_format in File_Format_Dictionary:
             directory_path = Path(File_Format_Dictionary[final_file_format])
             os.makedirs(directory_path, exist_ok=True)
@@ -60,4 +61,5 @@ def organizer():
                 print(f"Failed to move {file_path} to Others: {e}")
 
 if __name__ == "__main__":
+    save_undo_log()
     organizer()
